690dcac09354137336b93955bdb4d73736df99ba
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-02-19 09:56:20 +0200

    update Session_4/10_AritmeticOverloading.cpp

--- Session_4/10_AritmeticOverloading.cpp
+++ Session_4/10_AritmeticOverloading.cpp
@@ -68,6 +68,8 @@ public:
         m_number -= other.m_number;
     }
 
+    /*--------------------------------------*/
+
     bool operator==(CInteger& other) {
         return m_number == other.m_number;
     }
@@ -76,9 +78,38 @@ public:
         return m_number != other.m_number;
     }
 
-    int operator-() {
-        return -m_number;
+    bool operator<(CInteger& other) {
+        return m_number < other.m_number;
     }
+
+    bool operator>(CInteger& other) {
+        return m_number > other.m_number;
+    }
+
+    bool operator<=(CInteger& other) {
+        return m_number <= other.m_number;
+    }
+
+    bool operator>=(CInteger& other) {
+        return m_number >= other.m_number;
+    }
+
+    /*--------------------------------------*/
+    int operator!() {
+        return !m_number;
+    }
+
+    int operator&&(CInteger& other) {
+        return m_number && other.m_number;
+    }
+
+    int operator||(CInteger& other) {
+        return m_number || other.m_number;
+    }
+
+    /*--------------------------------------*/
+
+
 
     void print(){
         std::cout << "Number: " << m_number << std::endl;
@@ -102,12 +133,22 @@ int main(){
     int postIncrement = num1++;
     int preDecrement = --num2;
     int postDecrement = num2--;
+    int notValue = !num1;
+    int andValue = num1 && num2;
+    int orValue = num1 || num2;
 
     std::cout << "Sum: " << sum << std::endl;
     std::cout << "Difference: " << difference << std::endl;
     std::cout << "Product: " << product << std::endl;
     std::cout << "Quotient: " << quotient << std::endl;
     std::cout << "Modulo: " << modulo << std::endl;
+    std::cout << "Pre-Increment: " << preIncrement << std::endl;
+    std::cout << "Post-Increment: " << postIncrement << std::endl;
+    std::cout << "Pre-Decrement: " << preDecrement << std::endl;
+    std::cout << "Post-Decrement: " << postDecrement << std::endl;
+    std::cout << "Not Value: " << notValue << std::endl;
+    std::cout << "And Value: " << andValue << std::endl;
+    std::cout << "Or Value: " << orValue << std::endl;
 
     return 0;
 }

2268f17f96e8d296058c4607f55721caef6f150e
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-12 02:09:18 +0200

    feat: add initial implementation of union concepts in Basics.cpp

---
+++ CPP/Practice/03_Unions/Basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* Key Concepts
+** Shared Memory: All members share the same memory location
+** Size: Union size = size of largest member
+** Mutual Exclusivity: Only one member can hold a value at a time
+** Use Cases: Type punning, space-efficient data storage
+*/
+
+// Basic Union Declaration
+#if 1
+
+
+#endif

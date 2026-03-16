1f8c48c0870b919ab0b45ac84f8341ce3270fc2f
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-16 13:37:04 +0200

    refactor: Change Example5 for readability

--- QT/Examples/Example5/main.cpp
+++ QT/Examples/Example5/main.cpp
@@ -3,6 +3,8 @@
 #include <QQmlContext>
 #include <QQuickStyle>
 #include "downloaderThread.hpp"
+#include "sensorWorker.hpp"
+#include "sensorController.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -10,10 +12,11 @@ int main(int argc, char *argv[])
     #if 1
     QGuiApplication a(argc, argv);
 
-    DownloaderThread downloaderThread("example.com");
+    sensorController controller;
 
     QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("downloaderThread", &downloaderThread);
+    engine.rootContext()->setContextProperty("sensorWorker", &worker);
+    engine.rootContext()->setContextProperty("sensorController", &controller);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if(engine.rootobjects().isEmpty())
         return -1;

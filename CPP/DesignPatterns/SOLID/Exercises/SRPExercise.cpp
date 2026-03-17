67bef2373ca97ecb43c431f5eca3f2721987497b
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-17 11:27:39 +0200

    feat: add SRPExercise with FileLogger, MessageFormatter, RemoteLogSender, and LogCompressor classes

---
+++ CPP/DesignPatterns/SOLID/Exercises/SRPExercise.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+class FileLogger {
+public:
+    void logInfo(const std::string& message) {
+        std::ofstream file("log.txt", std::ios::app);
+        file << "INFO: " << message << std::endl;
+        file.close();
+    }
+};
+
+class MessageFormatter {
+public:
+    std::string formatMessage(const std::string& message) {
+        return "[Formatted] " + message;
+    }
+};
+
+class RemoteLogSender {
+public:
+    void sendMessage(const std::string& message) {
+        std::cout << "Sending: " << message << std::endl;
+    }
+};
+
+class LogCompressor {
+public:
+    std::string compressMessage(const std::string& message) {
+        return "Compressed(" + message + ")";
+    }
+};

db3869682d2410fcc7f8e662873108bf88512fcd
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-17 13:04:05 +0200

    style: improve code formatting for consistency in SRPExercise.cpp

--- CPP/DesignPatterns/SOLID/Exercises/SRPExercise.cpp
+++ CPP/DesignPatterns/SOLID/Exercises/SRPExercise.cpp
@@ -3,32 +3,40 @@
 #include <sstream>
 #include <fstream>
 
-class FileLogger {
+class FileLogger
+{
 public:
-    void log(const std::string& message) {
+    void log(const std::string &message)
+    {
         std::ofstream file("log.txt", std::ios::app);
         file << "INFO: " << message << std::endl;
         file.close();
     }
 };
 
-class MessageFormatter {
+class MessageFormatter
+{
 public:
-    std::string formatMessage(const std::string& message) {
+    std::string formatMessage(const std::string &message)
+    {
         return "[Formatted] " + message;
     }
 };
 
-class RemoteLogSender {
+class RemoteLogSender
+{
 public:
-    void sendMessage(const std::string& message) {
+    void sendMessage(const std::string &message)
+    {
         std::cout << "Sending: " << message << std::endl;
     }
 };
 
-class LogCompressor {
+class LogCompressor
+{
 public:
-    std::string compressMessage(const std::string& message) {
+    std::string compressMessage(const std::string &message)
+    {
         return "Compressed(" + message + ")";
     }
 };

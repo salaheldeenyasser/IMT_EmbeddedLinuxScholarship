e7cd123df45dc53ac03642b3cc9f9c31e0c38943
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-17 13:03:47 +0200

    style: format code for consistency and readability in LSPExercise.cpp

--- CPP/DesignPatterns/SOLID/Exercises/LSPExercise.cpp
+++ CPP/DesignPatterns/SOLID/Exercises/LSPExercise.cpp
@@ -1,62 +1,71 @@
 #include <iostream>
 
-
-class Creature{
-    public:
-    virtual void move() = 0;    
+class Creature
+{
+public:
+    virtual void move() = 0;
     virtual ~Creature() = default;
 };
 
-
-class seaCreature : public Creature{
-    public:
-    void move() override{
+class seaCreature : public Creature
+{
+public:
+    void move() override
+    {
         std::cout << "The sea creature swims." << std::endl;
     }
 };
 
-
-class landCreature : public Creature{
-    public:
-    void makeSound(){
+class landCreature : public Creature
+{
+public:
+    void makeSound()
+    {
         std::cout << "The land creature makes a sound." << std::endl;
     }
-    void move() override{
+    void move() override
+    {
         std::cout << "The land creature walks." << std::endl;
     }
 };
 
-class fish : public seaCreature{
-    public:
-    void move() override{
+class fish : public seaCreature
+{
+public:
+    void move() override
+    {
         std::cout << "The fish swims." << std::endl;
     }
 };
 
-class dog : public landCreature{
-    public:
-    void move() override{
+class dog : public landCreature
+{
+public:
+    void move() override
+    {
         std::cout << "The dog walks." << std::endl;
     }
 };
 
-void moveCreature(Creature* creature){
+void moveCreature(Creature *creature)
+{
     creature->move();
 }
 
-void makeSound(landCreature* creature){
+void makeSound(landCreature *creature)
+{
     creature->makeSound();
 }
 
-
-int main(){
-    Creature* myFish = new fish();
-    Creature* myDog = new dog();
+int main()
+{
+    Creature *myFish = new fish();
+    Creature *myDog = new dog();
 
     moveCreature(myFish);
     moveCreature(myDog);
 
-    makeSound(dynamic_cast<landCreature*>(myDog));
+    makeSound(dynamic_cast<landCreature *>(myDog));
 
     return 0;
 }

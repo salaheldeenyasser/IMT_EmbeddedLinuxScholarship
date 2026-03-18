a77f5124fe1672eedfc4a9f649b10bb7e8172724
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-18 14:48:57 +0200

    style: adjust access specifiers for HomeSystemManager and enhance DeviceConfig methods

--- DesignPatterns/PracticeProjects/SmartHomeDesignPatterns/SmartHome.cpp
+++ DesignPatterns/PracticeProjects/SmartHomeDesignPatterns/SmartHome.cpp
@@ -5,7 +5,7 @@
 
 class HomeSystemManager
 {
-    public:
+public:
     static HomeSystemManager &getInstance()
     {
         static HomeSystemManager instance;
@@ -30,7 +30,7 @@ class HomeSystemManager
         std::cout << "Event log for " << homeName << ":" << std::endl;
     }
 
-    private:
+private:
     static HomeSystemManager *instance;
     HomeSystemManager() : homeName("My Smart Home"), totalDevices(0), totalActions(0) {}
     ~HomeSystemManager() = default;
@@ -38,11 +38,66 @@ class HomeSystemManager
     int totalDevices;
     int totalActions;
     std::mutex mutex;
-
 };
 
 // Device Config (Builder pattern)
 
 class DeviceConfig
 {
+public:
+    void setName(const std::string &name)
+    {
+        this->name = name;
+    }
 
+    void setType(const std::string &type)
+    {
+        this->type = type;
+    }
+
+    void setRoom(const std::string &room)
+    {
+        this->room = room;
+    }
+
+    void setPower(int power)
+    {
+        this->power = power;
+    }
+
+    void setIP(const std::string &ip)
+    {
+        this->ip = ip;
+    }
+
+    void setOnline(bool isOnline)
+    {
+        this->isOnline = isOnline;
+    }
+
+    void setSchedule(bool schedule)
+    {
+        this->schedule = schedule;
+    }
+
+    void setScheduleOn(const std::string &scheduleOn)
+    {
+        this->scheduleOn = scheduleOn;
+    }
+
+    void setScheduleOff(const std::string &scheduleOff)
+    {
+        this->scheduleOff = scheduleOff;
+    }
+
+private:
+    std::string name;
+    std::string type;
+    std::string room;
+    int power = 0;
+    std::string ip = "192.168.1.1";
+    bool isOnline = true;
+    bool schedule = false;
+    std::string scheduleOn;
+    std::string scheduleOff;
+};

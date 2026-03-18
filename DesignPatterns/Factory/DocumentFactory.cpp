4b9cb25153a5a1300230cf9131eabc084fcb30e7
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-18 12:55:21 +0200

    Implement Factory Design Pattern for Document Creation
    - Added IDocument interface with render method.
    - Created concrete document classes: PDFDocument, WordDocument, HTMLDocument, and MarkdownDocument.
    - Developed DocumentCreator abstract class with a method to render documents.
    - Implemented specific document creators: PDFDocumentCreator, WordDocumentCreator, HTMLDocumentCreator, and MarkdownDocumentCreator.
    - Main function demonstrates the creation and rendering of different document types.

---
+++ DesignPatterns/Factory/DocumentFactory.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <memory>
+
+class IDocument
+{
+public:
+    virtual void render() = 0;
+    virtual ~IDocument() = default;
+};
+
+class PDFDocument : public IDocument
+{
+public:
+    void render() override
+    {
+        std::cout << "Rendering PDF" << std::endl;
+    }
+};
+
+class WordDocument : public IDocument
+{
+public:
+    void render() override
+    {
+        std::cout << "Rendering Word Document" << std::endl;
+    }
+};
+
+class HTMLDocument : public IDocument
+{
+public:
+    void render() override
+    {
+        std::cout << "Rendering HTML Document" << std::endl;
+    }
+};
+
+
+class DocumentCreator
+{
+public:
+    virtual std::unique_ptr<IDocument> createDocument() = 0;
+    virtual ~DocumentCreator() = default;
+
+    void renderDocument()
+    {
+        auto document = createDocument();
+        if (document)
+        {
+            document->render();
+        }
+        else
+        {
+            std::cout << "Unknown document type "  << std::endl;
+        }
+    }
+};
+
+class PDFDocumentCreator : public DocumentCreator
+{
+public:
+    std::unique_ptr<IDocument> createDocument() override
+    {
+        return std::make_unique<PDFDocument>();
+    }
+};  
+
+class WordDocumentCreator : public DocumentCreator
+{
+public:
+    std::unique_ptr<IDocument> createDocument() override
+    {        
+        return std::make_unique<WordDocument>();
+    }
+};
+
+class HTMLDocumentCreator : public DocumentCreator
+{
+public:    
+    std::unique_ptr<IDocument> createDocument() override
+    {        
+        return std::make_unique<HTMLDocument>();
+    }
+};
+
+
+class MarkdownDocument : public IDocument
+{
+public:
+    void render() override
+    {
+        std::cout << "Rendering Markdown Document" << std::endl;
+    }
+};
+
+class MarkdownDocumentCreator : public DocumentCreator
+{
+public:    
+    std::unique_ptr<IDocument> createDocument() override
+    {        
+        return std::make_unique<MarkdownDocument>();
+    }
+};
+
+
+int main()
+{
+    PDFDocumentCreator pdfCreator;
+    WordDocumentCreator wordCreator;
+    HTMLDocumentCreator htmlCreator;
+    MarkdownDocumentCreator markdownCreator;
+
+    pdfCreator.renderDocument();
+    wordCreator.renderDocument();
+    htmlCreator.renderDocument();
+    markdownCreator.renderDocument();
+
+    return 0;
+}
